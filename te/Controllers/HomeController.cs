3f42ad2a7a998debcf1140a0682eb2267389ee73
test <test@DESKTOP-U7LJSE9>
2022-06-30 18:45:32 +0530

    Add project files.

---
+++ te/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using te.Models;
+using te.ViewModels;
+using Kendo.Mvc.UI;
+using Kendo.Mvc.Extensions;
+
+namespace te.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        DatabaseContext db = new DatabaseContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetUser([DataSourceRequest] DataSourceRequest request)
+        {
+            IEnumerable<Usermodel> userlist = (from obj in db.tblusers
+                                               select new Usermodel()
+                                               {
+                                                   userid=obj.userid,
+                                                   email=obj.email,
+                                                   mobile=obj.mobile,
+                                                   password=obj.password,
+                                                   dob=obj.dob
+                                               }).ToList();
+            return Json(userlist.ToDataSourceResult(request), JsonRequestBehavior.AllowGet);
+        }
+
+        public void AddEmployee(tbluser emp)
+        {
+            db.tblusers.Add(emp);
+            db.SaveChanges();
+
+           
+        }
+        [HttpPost]
+        public void UpdateEmployee(tbluser emp)
+        {
+            tbluser obj = db.tblusers.FirstOrDefault(x=>x.userid==emp.userid);
+
+            obj.userid = emp.userid;
+            obj.email = emp.email;
+            obj.mobile = emp.mobile;
+            obj.password = emp.password;
+            obj.dob = emp.dob;
+            db.Entry(obj).State = System.Data.EntityState.Modified;
+            db.SaveChanges();
+        }
+
+        public void DeleteEmployee(tbluser emp)
+        {
+            var data = db.tblusers.Find(emp.userid);
+            db.tblusers.Remove(data);
+            db.SaveChanges();
+        }
+    }
+}
---
+++ te/ViewModels/Usermodel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace te.ViewModels
+{
+    public class Usermodel
+    {
+        public int userid { get; set; }
+        public string email { get; set; }
+        public int mobile { get; set; }
+        public string password { get; set; }
+        public DateTime dob { get; set; }
+    }
+}
